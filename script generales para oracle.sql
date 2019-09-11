c43dec798fd20ffe7bf29780a941c74dde99ddc6
jponce <contacto.jponce@gmail.com>
2019-09-10 22:37:28 -0400

    first commit

--- script generales para oracle.sql
+++ script generales para oracle.sql
@@ -3,10 +3,12 @@
 create tablespace tbs_oracle_demos datafile 'tbs_oracle_demos.dat' size 20M online;
 
 // create un usuario y lo asginamos al tablespace creado
-create user user_app_demo identified by "00.jponce" default tablespace tbs_oracle_demos;
+create user user_app_demo identified by "laclavedepapa" default tablespace tbs_oracle_demos;
 
 // entregamos persimos al usuario
 GRANT CONNECT, RESOURCE, DBA TO user_app_demo; 
 
 // aplicamos cambios
 commit
+
+
